457b9760afe31a4e639a951a059ae9781d109118
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 19:02:13 +0300

    function templates

--- python_password_manager.py
+++ python_password_manager.py
@@ -1,9 +1,16 @@
 pwd = input("What is the master password?")
+
+def add():
+    pass
+def view():
+    pass
+
 while True:
-    mode = input("a for add a new password, v for view a password")
-    if mode == "a":
-        pass
-    elif mode == "v":
-        pass
+    mode = input("type add for add a new password or type view for view a password").lower()
+    if mode == "add":
+        view()
+    elif mode == "view":
+        view()
     else:
         print("Invalid Input")
+        continue
