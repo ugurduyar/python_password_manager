c11a48626857cdc0715ef692c648795097adfd41
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 19:09:06 +0300

    password file added

--- python_password_manager.py
+++ python_password_manager.py
@@ -1,12 +1,18 @@
-pwd = input("What is the master password?")
+master_pwd = input("What is the master password? ")
 
 def add():
     pass
 def view():
-    pass
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open("passwords.txt", 'a') as f:
+        f.write(name + "|" + pwd + "\n")
 
 while True:
-    mode = input("type add for add a new password or type view for view a password").lower()
+    mode = input("type add for add a new password or type view for view a password ").lower()
+    if mode == "quit":
+        break
     if mode == "add":
         view()
     elif mode == "view":
