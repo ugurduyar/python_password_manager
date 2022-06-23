7a18cec34abaf6c9a6bc160ebbe1e442ba0e33f8
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 19:14:56 +0300

    rstrip

--- python_password_manager.py
+++ python_password_manager.py
@@ -1,16 +1,16 @@
 master_pwd = input("What is the master password? ")
 
+def view():
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            print(line.rstrip())
+            
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt", 'a') as f:
         f.write(name + "|" + pwd + "\n")
-
-def view():
-    with open("passwords.txt", 'r') as f:
-        for line in f.readlines():
-            print(line)
 
 while True:
     mode = input("type add for add a new password or type view for view a password ").lower()
