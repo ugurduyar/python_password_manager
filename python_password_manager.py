54eb3f0f3bdae74ec7fd8b6154defb5f6735e528
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 19:12:46 +0300

    huge mistake fixed

--- python_password_manager.py
+++ python_password_manager.py
@@ -1,22 +1,23 @@
 master_pwd = input("What is the master password? ")
 
 def add():
-    with open("passwords.txt", 'r') as f:
-        for line in f.readlines():
-            print(line)
-def view():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt", 'a') as f:
         f.write(name + "|" + pwd + "\n")
 
+def view():
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            print(line)
+
 while True:
     mode = input("type add for add a new password or type view for view a password ").lower()
     if mode == "quit":
         break
     if mode == "add":
-        view()
+        add()
     elif mode == "view":
         view()
     else:
