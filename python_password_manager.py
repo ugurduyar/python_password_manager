7a9e69ffbba5cc05c5c7c24fa15afe4ccafa2a64
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 18:54:03 +0300

    added mods

--- python_password_manager.py
+++ python_password_manager.py
@@ -1,1 +1,9 @@
-test = 123
+pwd = input("What is the master password?")
+while True:
+    mode = input("a for add a new password, v for view a password")
+    if mode == "a":
+        pass
+    elif mode == "v":
+        pass
+    else:
+        print("Invalid Input")
