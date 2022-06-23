414834e270299518245a20cff5ee386688ac862c
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 20:50:33 +0300

    check generated key

--- python_password_manager.py
+++ python_password_manager.py
@@ -1,11 +1,18 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
+
+def load_key():
+    file = open ("key.key","rb")
+    key = file.read()
+    file.close
+    return key
+
+key = load.key()
+master_pwd = input("What is the master password? ")
 
 def view():
     with open("passwords.txt", 'r') as f:
